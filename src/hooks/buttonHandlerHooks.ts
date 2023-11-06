1e3f7fa558c6afcdf4339fa9f07147065f281cf2
Yab <yabuman46@gmail.com>
2023-11-06 11:28:05 +0300

    Pattern update to match some edge cases

--- src/hooks/buttonHandlerHooks.ts
+++ src/hooks/buttonHandlerHooks.ts
@@ -106,7 +106,8 @@ export const useHandleSubmit = (
 
     if (handle.length > 0 && descriptionInput != "") {
       //This line of code insures all handles are of matdone format
-      const pattern = /^@[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.[a-zA-Z0-9_]+$/;
+      const pattern =
+        /^@[a-zA-Z0-9_!#$%^&*()+=\-[\]{}|\\:;"'<>,.?/]+@[a-zA-Z0-9_!#$%^&*()+=\-[\]{}|\\:;"'<>,.?/]+\.[a-zA-Z0-9_!#$%^&*()+=\-[\]{}|\\:;"'<>,.?/]+$/;
       const isValid = handle.every((user) => {
         console.log("User: ", user, " Valid: ", pattern.test(user));
 
