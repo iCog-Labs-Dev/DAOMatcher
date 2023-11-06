7426d7169fa346f14cd6f250ebfefe444f6c1c4b
Yab <yabuman46@gmail.com>
2023-11-06 10:29:00 +0300

    Multiple connections bug on depth change

--- src/Components/Body/Body.tsx
+++ src/Components/Body/Body.tsx
@@ -211,7 +211,7 @@ function Body({ isLoggedIn }: { isLoggedIn: boolean }) {
       return;
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoading ? success : isLoading, depth]);
+  }, [isLoading ? success : isLoading]);
 
   // useEffect(() => {
   //   const milliseconds = depth * 3474;
