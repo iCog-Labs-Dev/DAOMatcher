c0b85077b9270429cbdfbd8f597a0830ce62b2ea
Yab <yabuman46@gmail.com>
2024-04-18 14:23:29 +0300

    feat: Added state persistor

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "@/App.tsx";
 import "@/index.css";
 import { Provider } from "react-redux";
-import store from "@/redux/store";
+import store, { persistor } from "@/redux/store";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}></PersistGate>
       <App />
     </Provider>
   </React.StrictMode>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import homeReducer from "@/pages/Home/homeSlice";
 import usersReducer from "@/pages/Home/usersSlice";
 import userReducer from "@/redux/userSlice";
 import errorReducer from "@/redux/errorSlice";
 import infoReducer from "@/redux/infoSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  home: homeReducer,
+  errors: errorReducer,
+  users: usersReducer,
+  infoMessages: infoReducer,
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    home: homeReducer,
-    errors: errorReducer,
-    users: usersReducer,
-    infoMessages: infoReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
