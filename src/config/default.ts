f43eb12262199228dbce3536743fcfb0ff780e72
Yab <yabuman46@gmail.com>
2024-04-19 15:27:14 +0300

    chore: Removed unused code

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,13 +1,2 @@
-import { io, Socket } from "socket.io-client";
-
 // export const BASE_URL = "https://daomatcher-backend-v2.onrender.com";
 export const BASE_URL = "http://localhost:8000";
-
-export const getSocket = (token: string): Socket => {
-  const socket = io(`${BASE_URL}`, {
-    query: {
-      token,
-    },
-  });
-  return socket;
-};
