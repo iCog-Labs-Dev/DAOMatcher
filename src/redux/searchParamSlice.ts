64415e656803cd8cc4bbe763861ee03185484b22
Yab <yabuman46@gmail.com>
2024-05-05 09:55:37 +0300

    feat: Added search slice

---
+++ src/redux/searchParamSlice.ts
@@ -0,0 +1,56 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "@/redux/store";
+
+interface SearchParams {
+  query: string;
+  user_list: string[];
+  user_limit: number;
+  depth: number;
+}
+
+interface SearchSlice {
+  resubmitCount: number;
+  searchParam: SearchParams;
+}
+
+const initialState: SearchSlice = {
+  searchParam: { query: "", user_list: [], user_limit: 0, depth: 0 },
+  resubmitCount: 0,
+};
+
+const searchParamSlice = createSlice({
+  name: "searchParameters",
+  initialState,
+  reducers: {
+    addSearchParam: (state, action) => {
+      if (action.payload) {
+        state.searchParam.query = action.payload.query;
+        state.searchParam.user_list = action.payload.user_list;
+        state.searchParam.user_limit = action.payload.user_limit;
+        state.searchParam.depth = action.payload.depth;
+      } else {
+        console.log(
+          "Invalid payload received while adding user state in userSlice"
+        );
+      }
+    },
+    incrementResubmitCount: (state) => {
+      state.resubmitCount += 1;
+    },
+    clearSearchParam: (state) => {
+      state.searchParam.query = initialState.searchParam.query;
+      state.searchParam.user_list = initialState.searchParam.user_list;
+      state.searchParam.user_limit = initialState.searchParam.user_limit;
+      state.searchParam.depth = initialState.searchParam.depth;
+      state.resubmitCount = 0;
+    },
+  },
+});
+// export const selectSearchParams = (state: RootState) =>
+//   state.searchParameters.searchParam;
+// export const selectResubmitCount = (state: RootState) =>
+//   state.searchParameters.resubmitCount;
+// export const { addSearchParam, clearSearchParam, incrementResubmitCount } =
+//   searchParamSlice.actions;
+
+export default searchParamSlice.reducer;
