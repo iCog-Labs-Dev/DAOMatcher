0066705c924f56c48025ee8ccf42c410f7fb6109
eyuuab <eyobedabreham9@gmail.com>
2024-05-22 01:43:00 +0300

    hisoty page added

--- src/pages/Home/components/History.tsx
+++ src/pages/Home/components/History.tsx
@@ -1,22 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { selectToken } from "@/redux/userSlice";
 import { useSelector } from "react-redux";
-import { Card, CardContent, Typography, Button, styled, Box } from "@mui/material";
+import { Card, CardContent, Typography, Button, styled, Box, Avatar, CircularProgress } from "@mui/material";
 import axios from "axios";
-import { selectUser } from "@/redux/userSlice";
+import { selectToken, selectUser } from "@/redux/userSlice";
 import { RootState } from "@/redux/store";
 import { BASE_URL } from "@/config/default";
-import UsersList from "@/pages/Home/components/UsersList";
 
 const StyledCard = styled(Card)(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  maxWidth: '90%',
-  margin: '20px auto', 
+  width: '95%', // Increase card width
+  margin: '20px auto',
   padding: '20px',
   [theme.breakpoints.up('sm')]: {
-    maxWidth: 600,
+    maxWidth: 700,
   },
 }));
 
@@ -24,6 +22,18 @@ const StyledButton = styled(Button)({
   marginTop: '20px',
   padding: '8px 16px',
   borderRadius: 15,
+});
+
+const UserCard = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  padding: '10px',
+  borderBottom: '1px solid #ddd',
+  width: '100%',
+});
+
+const UserAvatar = styled(Avatar)({
+  marginRight: '10px',
 });
 
 interface User {
@@ -45,27 +55,26 @@ interface HistoryItem {
 function History() {
   const user = useSelector((state: RootState) => selectUser(state));
   const [historyData, setHistoryData] = useState<HistoryItem[]>([]);
-  const [page, setPage] = useState(1);
-  const [size] = useState(5);
   const [loading, setLoading] = useState(false);
   const Token = useSelector((state: RootState) => selectToken(state));
+  const [visibleCounts, setVisibleCounts] = useState<number[]>([]);
 
   useEffect(() => {
     const fetchHistoryData = async () => {
       try {
+        setLoading(true);
         const authToken = Token;
         const response = await axios.get(
-          `${BASE_URL}/api/user/${user.id}/search-result?page=${page}&size=${size}`,
+          `${BASE_URL}/api/user/${user.id}/search-result`,
           {
             headers: {
               Authorization: `Bearer ${authToken}`,
             },
           }
         );
-        setHistoryData((prevData) => [
-          ...prevData,
-          ...response.data.data.reverse(),
-        ]);
+        const newHistoryData = response.data.data; // Reverse the order
+        setHistoryData(newHistoryData);
+        setVisibleCounts(new Array(newHistoryData.length).fill(5));
         setLoading(false);
       } catch (error) {
         console.error("Error fetching history data:", error);
@@ -73,62 +82,66 @@ function History() {
       }
     };
     fetchHistoryData();
-  }, [user.id, page, size, Token]);
+  }, [user.id, Token]);
 
-  const handleLoadMore = () => {
-    setPage((prevPage) => prevPage + 1);
-    setLoading(true);
+  const handleLoadMoreUsers = (index: number) => {
+    setVisibleCounts((prevCounts) => {
+      const newCounts = [...prevCounts];
+      newCounts[index] += 5;
+      return newCounts;
+    });
   };
-  
 
   return (
-    <>
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        marginTop="100px"
-      >
-        <Typography variant="h4" style={{ marginBottom: "20px" }}>
-          History
-        </Typography>
-        {historyData.length > 0 ? (
-          historyData.map((historyItem, index) => (
-            <StyledCard elevation={3} key={index}>
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  {historyItem.description}
-                </Typography>
-                <Typography variant="body2">
-                  {new Date(historyItem.time_stamp).toLocaleString()}
-                </Typography>
-                <Typography variant="body2">
-                  Users with similar interests:
-                </Typography>
-                <UsersList users={historyItem.user_results.slice(0, 5)} />
-                {historyItem.user_results.length > 5 && (
-                  <StyledButton
-                    variant="contained"
-                    color="primary"
-                    onClick={() =>
-                      setHistoryData((prevData) => {
-                        const newData = [...prevData];
-                        newData[index].user_results = [
-                          ...historyItem.user_results,
-                          ...historyItem.user_results.slice(5),
-                        ];
-                        return newData;
-                      })
-                    }
-                  >
-                    Load More
-                  </StyledButton>
-                )}
-              </CardContent>
-            </StyledCard>
-          ))
-        ) : (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      marginTop="100px"
+    >
+      <Typography variant="h4" style={{ marginBottom: "20px" }}>
+        History
+      </Typography>
+      {loading && <CircularProgress />}
+      {historyData.length > 0 ? (
+        historyData.map((historyItem, index) => (
+          <StyledCard elevation={3} key={index}>
+            <CardContent>
+              <Typography variant="h4" component="div" style={{ textTransform: 'capitalize' }}>
+                {historyItem.description}
+              </Typography>
+              <Typography variant="body2">
+                {new Date(historyItem.time_stamp).toLocaleString()}
+              </Typography>
+              <Typography variant="body2">
+                Users with similar interests:
+              </Typography>
+              {historyItem.user_results.slice(0, visibleCounts[index]).map((user, userIndex) => (
+                <UserCard key={userIndex}>
+                  <UserAvatar src={user.image_url} alt={user.username} />
+                  <Typography variant="body2">
+                    {user.username} ({user.handle})
+                  </Typography> 
+                  <Typography variant="body2">
+                    {user.score}%
+                  </Typography>
+                </UserCard>
+              ))}
+              {visibleCounts[index] < historyItem.user_results.length && (
+                <StyledButton
+                  variant="contained"
+                  color="primary"
+                  onClick={() => handleLoadMoreUsers(index)}
+                >
+                  Load More
+                </StyledButton>
+              )}
+            </CardContent>
+          </StyledCard>
+        ))
+      ) : (
+        !loading && (
           <StyledCard elevation={3}>
             <CardContent>
               <Typography variant="h5" component="div">
@@ -136,9 +149,9 @@ function History() {
               </Typography>
             </CardContent>
           </StyledCard>
-        )}
-      </Box>
-    </>
+        )
+      )}
+    </Box>
   );
 }
 
