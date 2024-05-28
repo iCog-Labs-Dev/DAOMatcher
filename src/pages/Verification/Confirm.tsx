58ea863899219625a028c1f4fcb739071901bb0d
Baslael Workineh <baslaworku@gmail.com>
2024-05-28 14:26:10 +0300

    Removed styling since it's deprecated

--- src/pages/Verification/Confirm.tsx
+++ src/pages/Verification/Confirm.tsx
@@ -1,51 +1,40 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Typography, CircularProgress } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import axios, { AxiosResponse } from 'axios';
-import { BASE_URL } from '@/config/default';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Typography, CircularProgress, Box } from "@mui/material";
+import axios, { AxiosResponse } from "axios";
+import { BASE_URL } from "@/config/default";
 import { selectToken } from "@/redux/userSlice";
 import { useSelector } from "react-redux";
-import AuthResponse from '@/types/AuthTypes'
+import AuthResponse from "@/types/AuthTypes";
 
-
-const useStyles = makeStyles(() => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    minHeight: '100vh',
-  },
-  message: {
-    marginTop: "2px",
-  },
-}));
-
-type ConfirmationStatus = 'pending' | 'success' | 'error';
+type ConfirmationStatus = "pending" | "success" | "error";
 
 function Confirm() {
-  const classes = useStyles();
   const navigate = useNavigate();
-  const [confirmationStatus, setConfirmationStatus] = useState<ConfirmationStatus>('pending');
+  const [confirmationStatus, setConfirmationStatus] =
+    useState<ConfirmationStatus>("pending");
   const authtoken = useSelector(selectToken);
   //useParam
-  const token = window.location.pathname.split('/').pop();
-  
+  const token = window.location.pathname.split("/").pop();
+
   useEffect(() => {
     const verifyToken = async () => {
       try {
-        const response: AxiosResponse<AuthResponse> = await axios.get(`${BASE_URL}/api/auth/confirm/${token}`, 
-        { withCredentials: true, headers: { Authorization: `Bearer ${authtoken}` }}
+        const response: AxiosResponse<AuthResponse> = await axios.get(
+          `${BASE_URL}/api/auth/confirm/${token}`,
+          {
+            withCredentials: true,
+            headers: { Authorization: `Bearer ${authtoken}` },
+          }
         );
-        
+
         if (response.status === 200) {
-          setConfirmationStatus('success');
+          setConfirmationStatus("success");
         } else {
-          setConfirmationStatus('error');
+          setConfirmationStatus("error");
         }
       } catch (error) {
-        setConfirmationStatus('error');
+        setConfirmationStatus("error");
       }
     };
 
@@ -53,26 +42,24 @@ function Confirm() {
   }, [token]);
 
   useEffect(() => {
-    if (confirmationStatus === 'success') {
-      navigate('/DAOMatcher');
+    if (confirmationStatus === "success") {
+      navigate("/DAOMatcher");
     }
   }, [confirmationStatus, navigate]);
 
   let ConfirmationMessageComponent;
   switch (confirmationStatus) {
-    case 'pending':
+    case "pending":
       ConfirmationMessageComponent = (
         <>
           <CircularProgress />
-          <Typography variant="body1" className={classes.message}>
-            Verifying...
-          </Typography>
+          <Typography variant="body1">Verifying...</Typography>
         </>
       );
       break;
-    case 'error':
+    case "error":
       ConfirmationMessageComponent = (
-        <Typography variant="body1" className={classes.message}>
+        <Typography variant="body1">
           Error confirming email. Please try again later.
         </Typography>
       );
@@ -82,10 +69,16 @@ function Confirm() {
   }
 
   return (
-    <div className={classes.root}>
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
       <Typography variant="h2">Email Confirmation</Typography>
       {ConfirmationMessageComponent}
-    </div>
+    </Box>
   );
 }
 
