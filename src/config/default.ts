f6924a4468d1479784c6e6493635865526423071
ishak <ishaksebsib@gmail.com>
2024-05-28 14:46:25 +0300

    navbar ui and funcitonality improved

--- src/config/default.ts
+++ src/config/default.ts
@@ -2,6 +2,13 @@ import { io } from "socket.io-client";
 
 export const BASE_URL = "https://daomatcher-backend-v2.onrender.com";
 // export const BASE_URL = "http://localhost:8000";
+
+export const CONSTANTS = {
+	URL: {
+		GET_RANDOM_IMAGE: "https://robohash.org",
+	},
+	NO_NAVBAR_PAGES:['/DAOMatcher/login', '/DAOMatcher/signup']
+};
 export const socket = io(BASE_URL, { autoConnect: false });
 export const RESUBMIT_COUNT_LIMIT = 5;
 
