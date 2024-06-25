aab6074e35d2c39d1e34f8293d4cd38c7c2384f3
Yab <yabuman46@gmail.com>
2024-06-25 13:30:46 +0300

    fix: Socket connection poll error

--- src/config/default.ts
+++ src/config/default.ts
@@ -4,17 +4,17 @@ export const BASE_URL = "https://daomatcher-backend-v2.onrender.com";
 // export const BASE_URL = "http://localhost:8000";
 
 export const CONSTANTS = {
-	URL: {
-		GET_RANDOM_IMAGE: "https://robohash.org",
-		PUT_PROFILE: "/api/user", // use it with /userid
-	},
-	PRIVATE_ROUTES: [
-		"/DAOMatcher",
-		"/DAOMatcher/history",
-		"/DAOMatcher/profile",
-	],
+  URL: {
+    GET_RANDOM_IMAGE: "https://robohash.org",
+    PUT_PROFILE: "/api/user", // use it with /userid
+  },
+  PRIVATE_ROUTES: [
+    "/DAOMatcher",
+    "/DAOMatcher/history",
+    "/DAOMatcher/profile",
+  ],
 };
-export const socket = io(BASE_URL, { autoConnect: false });
+export const socket = io(BASE_URL, { autoConnect: false, transports: ["websocket"] })
 export const RESUBMIT_COUNT_LIMIT = 5;
 
 export const TWITTER_PREFIX = "tw+";
